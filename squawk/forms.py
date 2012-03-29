343f00b234a35dc05d6509ea985c606a0d909023
Joe Boutros <jboutros@gmail.com>
2012-03-29 15:36:48 -0500

    add squawk form + view

---
+++ squawk/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.forms import ModelForm
+from models import Squawk
+
+class AddSquawkForm(forms.Form):
+    squawk = forms.CharField()
--- squawk/views.py
+++ squawk/views.py
@@ -1,10 +1,20 @@
 from models import Squawk
+from forms import AddSquawkForm
 from django.http import HttpResponse
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response
 
 def squawk_list(request):
+    if request.method == "POST":
+        add_form = AddSquawkForm(request.POST)
+        if add_form.is_valid():
+            s = Squawk()
+            s.text = add_form.cleaned_data['squawk']
+            s.user = request.user
+            s.save()
+    form = AddSquawkForm()
     squawks = Squawk.objects.all()
     c= {}
     c['squawks'] = squawks
+    c['form'] = form
     return render_to_response("home.html", c, context_instance=RequestContext(request))
