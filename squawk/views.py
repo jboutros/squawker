ac269cad04f1cdd0813ce43506b56f121c5463f7
Joe Boutros <jboutros@gmail.com>
2012-03-29 15:40:24 -0500

    auth for form, sort squawks

--- squawk/views.py
+++ squawk/views.py
@@ -13,7 +13,7 @@ def squawk_list(request):
             s.user = request.user
             s.save()
     form = AddSquawkForm()
-    squawks = Squawk.objects.all()
+    squawks = Squawk.objects.all().order_by('-id')
     c= {}
     c['squawks'] = squawks
     c['form'] = form
