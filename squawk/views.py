3c7c82d21df2ce46fa9253a6ac576761c7f5f9b9
Joe Boutros <jboutros@gmail.com>
2012-03-29 15:04:21 -0500

    added templates and first view

--- squawk/views.py
+++ squawk/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from models import Squawk
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def squawk_list(request):
+    return render_to_response(template_name="base.html")
