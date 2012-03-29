ecc634fd2e697507a51dc11cef6edf11db12d3fa
Joe Boutros <jboutros@gmail.com>
2012-03-29 14:05:46 -0500

    squawk model

--- squawk/models.py
+++ squawk/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Squawk(models.Model):
+    text = models.CharField(max_length=141)
+    user = models.ForeignKey(User)
