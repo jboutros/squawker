ad25ff08823d5ab79b6116742a93a5a96d145750
Joe Boutros <jboutros@gmail.com>
2012-07-02 12:03:34 -0500

    list view + template

--- squawk/views.py
+++ squawk/views.py
@@ -1,6 +1,10 @@
 from models import Squawk
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
  
 def squawk_list(request):
-    return render_to_response(template_name="base.html")
+    squawks = Squawk.objects.all()
+    c= {}
+    c['squawks'] = squawks
+    return render_to_response("home.html", c, context_instance=RequestContext(request))
